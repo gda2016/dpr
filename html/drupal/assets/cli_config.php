f85d926350684179f053904863245e54eba8a3b6
root <root@localhost.localdomain>
2016-07-27 06:50:06 +0300

    00

---
+++ html/drupal/assets/cli_config.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+	'server' => 'http://online.autoeuro.ru/ae_server/srv_main.php',
+	'client_name' => 'autosfera2013@gmail.com',
+	'client_pwd' => 'lwVQ69O0jGSgAqyDmIsKWFbUrXRM7Nv5',
+);
+?>
