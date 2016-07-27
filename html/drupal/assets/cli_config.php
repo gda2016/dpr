359e1b0cc9fe2b405812cac6fffeba359c1978a6
David <ora55445@gmail.com>
2016-07-27 07:25:31 +0300

    Delete cli_config.php

--- html/drupal/assets/cli_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array (
-	'server' => 'http://online.autoeuro.ru/ae_server/srv_main.php',
-	'client_name' => 'autosfera2013@gmail.com',
-	'client_pwd' => 'lwVQ69O0jGSgAqyDmIsKWFbUrXRM7Nv5',
-);
-?>
